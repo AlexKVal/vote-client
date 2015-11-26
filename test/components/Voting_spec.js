5abb007d9e1993430ffb7922f309dffcb10e514a
AlexKVal <alexkval@gmail.com>
2015-11-26 16:41:45 +0200

    [added] pure-render-mixin

--- test/components/Voting_spec.js
+++ test/components/Voting_spec.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import ReactTestUtils from 'react/lib/ReactTestUtils';
+import {List} from 'immutable';
 import {expect} from 'chai';
 
 import Voting from '../../src/components/Voting';
@@ -56,8 +58,39 @@ describe('Voting', () => {
     const buttons = ReactTestUtils.scryRenderedDOMComponentsWithTag(component, 'button');
     expect(buttons.length).to.equal(0);
 
-    const winner = React.findDOMNode( component.refs.winner );
+    const winner = ReactDOM.findDOMNode( component.refs.winner );
     expect(winner).to.be.ok;
     expect(winner.textContent).to.contain('Trainspotting');
+  })
+
+  // use benefits of immutability
+  it('renders as a pure component', () => {
+    const pair = ['first', 'second'];
+    const component = ReactTestUtils.renderIntoDocument(
+      <Voting pair={pair} />
+    );
+
+    let firstButton = ReactTestUtils.scryRenderedDOMComponentsWithTag(component, 'button')[0];
+    expect(firstButton.textContent).to.equal('first');
+
+    pair[0] = 'Third';
+    component.setProps({pair});
+    firstButton = ReactTestUtils.scryRenderedDOMComponentsWithTag(component, 'button')[0];
+    expect(firstButton.textContent).to.equal('first');
+  })
+
+  it('does update DOM when prop changes', () => {
+    const pair = List.of('first', 'second');
+    const component = ReactTestUtils.renderIntoDocument(
+      <Voting pair={pair} />
+    );
+
+    let firstButton = ReactTestUtils.scryRenderedDOMComponentsWithTag(component, 'button')[0];
+    expect(firstButton.textContent).to.equal('first');
+
+    const newPair = pair.set(0, 'Third');
+    component.setProps({pair: newPair});
+    firstButton = ReactTestUtils.scryRenderedDOMComponentsWithTag(component, 'button')[0];
+    expect(firstButton.textContent).to.equal('Third');
   })
 });
