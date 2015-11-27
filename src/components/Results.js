f9eb039a9ff75daf35ba79a467bc416e224c9409
AlexKVal <alexkval@gmail.com>
2015-11-27 19:12:12 +0200

    [added] 'Results' now shows tally

--- src/components/Results.js
+++ src/components/Results.js
@@ -8,12 +8,23 @@ export default React.createClass({
     return this.props.pair || [];
   },
 
+  getVotes: function(entry) {
+    if (this.props.tally && this.props.tally.has(entry)) {
+      return this.props.tally.get(entry);
+    }
+
+    return 0;
+  },
+
   render: function() {
     return <div className="results">
       {
         this.getPair().map( entry =>
           <div key={entry} className="entry">
             <h1>{entry}</h1>
+            <div className="voteCount">
+              {this.getVotes(entry)}
+            </div>
           </div>
         )
       }
