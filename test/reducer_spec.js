8e1ad4c034329396a9cb81abed613e7080b9869d
AlexKVal <alexkval@gmail.com>
2015-11-29 13:27:45 +0200

    Add some additional specs for demonstration purpose

--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -24,4 +24,46 @@ describe('reducer', () => {
       }
     }))
   })
+
+  // just for demonstration purpose
+  if('handles SET_STATE with plain JS payload', () => {
+    const initialState = Map();
+    const action = {
+      type: 'SET_STATE',
+      state: Map({
+        vote: Map({
+          pair: List.of('first', 'second'),
+          tally: Map({first: 1})
+        })
+      })
+    };
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['first', 'second'],
+        tally: {first: 1}
+      }
+    }))
+  })
+
+  it('handles SET_STATE without initial state', () => {
+    const action = {
+      type: 'SET_STATE',
+      state: Map({
+        vote: Map({
+          pair: List.of('first', 'second'),
+          tally: Map({first: 1})
+        })
+      })
+    };
+    const nextState = reducer(undefined, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['first', 'second'],
+        tally: {first: 1}
+      }
+    }))
+  })
 })
