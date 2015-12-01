e81a4c33bbee7940b59e29eb50a961bf1961ae18
AlexKVal <alexkval@gmail.com>
2015-12-01 16:18:26 +0200

    [added] dispatch the 'state' received from the server

--- src/index.js
+++ src/index.js
@@ -11,17 +11,11 @@ import {VotingContainer} from './components/Voting';
 import {ResultsContainer} from './components/Results';
 
 const store = createStore(reducer);
-store.dispatch({
-  type: 'SET_STATE',
-  state: {
-    vote: {
-      pair: ['Sunshine', 'Trainspotting'],
-      tally: {Sunshine: 2}
-    }
-  }
-});
 
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on('state', (state) => {
+  store.dispatch({type: 'SET_STATE', state})
+})
 
 const routes = <Route component={App}>
   <Route path="/results" component={ResultsContainer} />
