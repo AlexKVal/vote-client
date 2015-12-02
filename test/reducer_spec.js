470fbdddf4ec040744a8fb5abe4b610d8d7d3ff7
AlexKVal <alexkval@gmail.com>
2015-12-02 14:50:51 +0200

    Add specs for the 'VOTE' action

--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -4,6 +4,7 @@ import {expect} from 'chai';
 import reducer from '../src/reducer';
 
 describe('reducer', () => {
+  // the first action
   it('handles SET_STATE', () => {
     const initialState = Map();
     const action = {
@@ -65,5 +66,43 @@ describe('reducer', () => {
         tally: {first: 1}
       }
     }))
+  })
+
+  // the second action
+  it('handles VOTE by setting hasVoted', () => {
+    const state = fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later'],
+        tally: {Trainspotting: 1}
+      }
+    });
+    const action = {type: 'VOTE', entry: 'Trainspotting'};
+    const nextState = reducer(state, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later'],
+        tally: {Trainspotting: 1}
+      },
+      hasVoted: 'Trainspotting'
+    }));
+  })
+
+  it('does not set hasVoted for VOTE on invalid entry', () => {
+    const state = fromJS({
+      vote: {
+        pair: ['first', 'second'],
+        tally: {first: 1}
+      }
+    });
+    const action = {type: 'VOTE', entry: 'another'};
+    const nextState = reducer(state, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['first', 'second'],
+        tally: {first: 1}
+      }
+    }));
   })
 })
